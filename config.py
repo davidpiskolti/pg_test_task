7cee84df5feca5dc547136c44c531782fb785052
davidpiskolti <david.piskolti@gmail.com>
2024-06-27 10:23:24 +0200

    config is extended with other properties.

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import yaml
-
 class Config:
     def __init__(self, config_path):
         with open(config_path, 'r') as file:
@@ -7,3 +6,12 @@ class Config:
     @property
     def input_paths(self):
         return self.config['input_paths']
+    @property
+    def output_path(self):
+        return self.config['output_path']
+    @property
+    def columns(self):
+        return self.config['columns']
+    @property
+    def aggregations(self):
+        return self.config['aggregations']
